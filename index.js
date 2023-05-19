ba0941fb30ca1f1b8feac308133a782b8b4bcce0
LI4NOST <xli4nost@mail.ru>
2023-05-19 13:38:15 +0300

    api work

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const buttons = document.querySelectorAll('.application_icon')
 const inputElem = document.querySelector('.application_input')
+const formElem = document.querySelector('.application_form')
+const submitButton = document.querySelector('.application_form__button')
+let clickButton= ""
+const defaultState = ()=>{
+    buttons[0].classList.add('button_active')
+    let clickButton= 'WhatsApp'
+}
+
 
 
 for (let button of buttons){
+    defaultState()
     button.addEventListener('click',()=>{
         let activeButton= document.querySelector('.application_icon.button_active')
         if(activeButton){
@@ -12,6 +21,36 @@ for (let button of buttons){
     })
 }
 
+const checkResponse =(res)=>{
+    if (res.ok){
+        alert('Заявка успешно отправлена! Мы свяжемся с вами в ближайшее время.')
+    }
+}
+
+const handleSubmit =(event)=>{
+    event.preventDefault()
+    const contact = inputElem.value
+    const active = document.querySelectorAll('.application_icon.button_active')
+    if(active){
+        active.forEach(elem=>{
+            clickButton=elem.textContent
+        })
+    }
+
+    fetch('https://upkeep.kz/api/v1/call', {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "type": `${clickButton}`,
+            "value":`${contact}`,
+        })
+    })
+
+        .then (res=>checkResponse(res))
+
+}
 
 
-
+submitButton.addEventListener('click', handleSubmit)
